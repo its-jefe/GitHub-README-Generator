b35c9e475a12e4a93cbdbc4b95e088eeb5eaa1ef
Jeff Jobs <Jeff.Timson@Yahoo.com>
2021-06-22 12:18:36 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+// https://github.com/coding-boot-camp/potential-enigma/blob/main/readme-guide.md
+
+// TODO 
+// Because this application won’t be deployed, 
+// you’ll also need to provide a link to a walkthrough 
+// video that demonstrates its functionality. 
+// Revisit 2.2.4: Screencastify Tutorial in Module 2 
+// of the prework as a refresher on how to record video 
+// from your computer. You’ll need to submit a link to 
+// the video and add it to the README of your project.
