ec10446ec83d7253cf1cb965bf984b6ea4eb7989
Jeff Jobs <Jeff.Timson@Yahoo.com>
2021-06-25 16:54:09 -0400

    lots of work

--- index.js
+++ index.js
@@ -1,20 +1,174 @@
+// TODO : // TRY NOT TO PASS THE WHOLE RESPONSE 
+
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateReadme = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Figure out how to allow the user to select pictures 
+// from the asset/images folder and use them as a preview
+const prompts = [
+    {
+        type: "input",
+        name: "title",
+        message: "Enter the title of your project: ",
+        default: '',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('You need to enter a project name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe your project: ",
+        default: '',
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the steps required to install your project: ",
+        default: '',
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide instructions and examples for use. ADD LINK TO SCREENSHOT?", // add a screenshot
+        default: '',
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "List your collaborators, if any, with links to their GitHub profiles.",
+        default: '',
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Link test files.", //Go the extra mile and write tests for your application. Then provide examples on how to run them.
+        default: '',
+    },
+    {
+        type: "confirm",
+        name: "license",
+        message: "Does your GitHub apppliation have a license? *ReadMe Generator will find it", // Wanted as a badge at the top "
+        default: false,
+        // does it work like that???
+        // choices: ['MIT', 'ISC', 'Apache'],
+    },
+    // {
+    //     type: "input",
+    //     name: "badges",
+    //     message: "",
+    //     default: "",
+    //     // choices: "",
+    //     // validate: "",
+    //     // when: ""
+    // },
+    // {
+    //     type: "input",
+    //     name: "features",
+    //     message: "",
+    //     default: "",
+    //     // choices: "",
+    //     // validate: "",
+    //     // when: ""
+    // },
+    // {
+    //     type: "input",
+    //     name: "contributors",
+    //     message: "",
+    //     default: "",
+    //     // choices: "",
+    //     // validate: "",
+    //     // when: ""
+    // },
+    {
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub username: ",
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('You need to enter your GitHub username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "repo",
+        message: "Enter your GitHub repo name: ",
+        default: '',
+        validate: repoInput => {
+            if (repoInput) {
+                return true;
+            } else {
+                console.log('You need to enter your GitHub repo name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your email: ",
+        default: '',
+    },
+]
+const promptUser = async () => {
+    let dataObject = {};
+    for (let i = 0; i < prompts.length; i++) {
+        await inquirer.prompt(
+            prompts[i]
+        ).then(data => {
+            if (data[prompts[i].name]) { // if the prompt was answered
+                dataObject[prompts[i].name] = data[prompts[i].name];
+            }
+        })
+    }
+    return dataObject;
+};
 
+// include a ToC if (sections > 2) or something 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = data => {
+    let markdown = generateReadme(data);
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/readme.md', markdown, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    return promptUser();
+}
+
+const failureCallback = (err) => {
+    console.log(`Error : ${err}`);
+}
 
 // Function call to initialize app
-init();
+init()
+    .then(data => { writeToFile(data) }, failureCallback)
 
 // https://github.com/coding-boot-camp/potential-enigma/blob/main/readme-guide.md
 
-// TODO 
+// TODO:
 // Because this application won’t be deployed, 
 // you’ll also need to provide a link to a walkthrough 
 // video that demonstrates its functionality. 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,141 @@
+// const {data, license} = arrayThatIPassInHere;
+// or just have data be an object and access license via data.license
+// although I should learn deconstructuring
+
+// EITHER I WILL INPORT FS HERE OR I CAN DO IT FROM INDEX - Choose clean code
+const fs = require('fs');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderBadges(username, repo, license) {
+  let ret = ""
+  ret += `
+![Analysis](https://img.shields.io/github/languages/top/${username}/${repo})`
+  if (license){
+    ret += `
+![License](https://img.shields.io/github/license/${username}/${repo})
+  `
+  }
+  return ret
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return `
+https://img.shields.io/github/license/${githubUsername}/${githubRepo}
+  `
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `
+    ${renderLicenseLink}
+  `
+  } else { return [] }
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const generateMarkdown = data => {
+  console.log("GENERATE MARKDOWN!")
+  console.log(data)
 
-`;
+  let addSections = "";
+
+  if (data.title) { //it's required!!
+    addSections += `
+# ${data.title}
+`
+  }
+
+  // add badges to top of readme
+  addSections += `
+${renderBadges(data.github, data.repo, data.license)}`
+
+
+  if (data.description) {
+    addSections += `
+## Description
+${data.description}
+`
+  }
+
+  // TABLE OF CONTENTS
+  addSections += tableOContents(data)
+
+  if (data.installation) {
+    addSections += `
+## Installation
+${data.installation}
+`
+  }
+
+  if (data.usage) {
+    addSections += `
+## Usage
+${data.usage}
+`
+  }
+
+  if (data.license) { // get into render license
+    addSections += `
+## License
+This application is covered under
+${renderLicenseLink}
+`
+  }
+
+  if (data.contributing) {
+    addSections += `
+## Contributing
+${data.contributing}
+`
+  }
+
+  if (data.tests) {
+    addSections += `
+## Tests
+${data.tests}
+`
+  }
+
+  if (data.github && data.title) { //github is required
+    addSections += `
+## Questions
+https://github.com/${data.github}/${data.repo}
+`
+  }
+
+  if (data.email) {
+    addSections += `
+If there are any additional questions send me an email! 
+
+${data.email}
+`
+  }
+  return addSections
+};
+
+// Creates Table of Contents
+function tableOContents(data) {
+  const content = (Object.entries(data))
+  let ret = "";
+  let i = 2; //cut title and description
+  ret += `
+## Table of Contents`
+  while (content[i][0] != 'github') {
+    console.log(content[i][0])
+    let header = content[i][0].charAt(0).toUpperCase() + content[i][0].slice(1);
+    ret += `
+* [${header}](#${content[i][0]})`
+    i++;
+  }
+  ret += `
+* [Questions](#questions)
+`
+  return ret;
 }
 
 module.exports = generateMarkdown;
